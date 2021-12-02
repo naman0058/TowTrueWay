f161cc183289b2b1f95431b4b6cb78b906892ed0
naman0058 <jnaman345@gmail.com>
2021-12-02 13:07:54 +0530

    a

--- routes/pool.js
+++ routes/pool.js
@@ -1,20 +1,16 @@
 
-
-  
 var mysql = require('mysql')
+require('dotenv').config()
 
 const pool = mysql.createPool({
  
-   host : 'db-mysql-blr1-56981-do-user-9676911-0.b.db.ondigitalocean.com',
-    user: 'doadmin',
-    password : 'a0xe66knpqoeoy0i',
-    database: 'e-commerce',
+ host : 'db-mysql-blr1-79982-do-user-10297867-0.b.db.ondigitalocean.com',
+   user: 'doadmin',
+    password : 'nc3PdpfQIcW8tOFm',
+    database: 'towtrueway',
     port:'25060',
     multipleStatements: true
   })
 
 
-
-
 module.exports = pool;
-
